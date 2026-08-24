91231f1727e9c793b098a7502a2fcfe13b8c602b
claridis117 <distefanoclari@gmail.com>
2026-08-24 12:42:56 -0300

    Avances en funcion main
    Creacion de bucle que recopila los datos del equipo
    Creacion de listas paralelas: nombres de los integrantes, rol del integrante

--- Codigo/perfil_equipo.py
+++ Codigo/perfil_equipo.py
@@ -23,5 +23,18 @@ def main():
     print("== Registro del equipo ==")
     
     nombre_equipo= input("Ingrese el nombre del equipo: ")
+    comision = input("Ingrese la comision: ")
+    cant_integrantes= int(input("Cuantos integrantes son?: "))
     
+    nombres_integrantes= []
+    roles_integrantes= []
+    
+    for i in range(cant_integrantes):
+        print(f"\n=== Integrante {i + 1}===")
+        nombre= input("Nombre: ")
+        rol= input("Rol en el equipo: ")
+        
+        nombres_integrantes.append(normalizar_nombre(nombre))
+        roles_integrantes.append(rol)
+        
 main()
